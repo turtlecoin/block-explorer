01c03c8939cf035b90b915e01bd83d6238ddc6b2
islandjim <35803056+islandjim@users.noreply.github.com>
2018-01-25 15:47:39 +0100

    Update networkStats and API address.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://45.32.232.11:32348';
+var api = 'YOUR_IP_AND_PORT_HERE';
 var blockTargetInterval = 30;
 var coinUnits = 100;
 var symbol = 'TRTL';
@@ -9,14 +9,13 @@ var networkStat = {
         ["eu.turtlepool.space", "http://eu.turtlepool.space:8117"],
         ["us.turtlepool.space", "http://us.turtlepool.space:8117"],
         ["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],
-        ["turtlecoinpool.ddns.net", "https://api.turtleco.in/turtlecoinpool.ddns.net"],
+        ["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
         ["slowandsteady.fun", "http://slowandsteady.fun:8119"],
         ["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
-        ["pool.turtleco.in", "https://pool.turtleco.in/api"],
-        ["auspool.turtleco.in", "https://auspool.turtleco.in/api/"],
-        ["trtl.radicaldelta.org", "https://api.turtleco.in/trtl.radicaldelta.org"],
+        ["auspool.turtleco.in", "https://auspool.turtleco.in/api"],
+        ["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
         ["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
-        ["ny.minetrtl.us", "https://api.turtleco.in/ny.minetrtl.us"],
+        ["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
         ["z-pool.com", "http://z-pool.com:8117"],
         ["xk.is", "https://xk.is/api"]
     ]	
@@ -24,6 +23,18 @@ var networkStat = {
 
 var networkStat2 = {
     "trtl": [
-		[]
+        ["eu.turtlepool.space", "http://eu.turtlepool.space:8117"],
+        ["us.turtlepool.space", "http://us.turtlepool.space:8117"],
+        ["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],
+        ["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
+        ["slowandsteady.fun", "http://slowandsteady.fun:8119"],
+        ["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
+        ["auspool.turtleco.in", "https://auspool.turtleco.in/api"],
+        ["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
+        ["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
+        ["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
+        ["z-pool.com", "http://z-pool.com:8117"],
+        ["xk.is", "https://xk.is/api"]
+
 		]
 };
